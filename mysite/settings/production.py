6fa8a28cb381225bb8b558fc865188ee59d0fde6
bastianl <bastian.lennart@gmail.com>
2017-03-07 10:09:00 -0400

    update production setting

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,4 +1,6 @@
 # config production db for heroku
-DATABASES['default'] = dj_database_url.config()
+import dj_database_url
+
+DATABASES = { 'default': dj_database_url.config() }
 
 ALLOWED_HOSTS = ['obscure-eyrie-90249.herokuapp.com']
